4332e01f848bb17c25268d6583aefec1ea9a40bb
Kian Acquoij <kian.acquoij@student.rocva.nl>
2023-09-28 10:06:24 +0200

    Fixed not being able to drop at bottom

--- resources/views/components/todo/card-item-container.blade.php
+++ resources/views/components/todo/card-item-container.blade.php
@@ -1,6 +1,6 @@
 @props(['tasks', 'card'])
 
-<ul id="dropzone-{{ $card->id }}" data-id="{{ $card->id }}" class="dropzone flex flex-col space-y-5 h-full mt-2 my-8 items-center">
+<ul id="dropzone-{{ $card->id }}" data-id="{{ $card->id }}" class="dropzone flex flex-col space-y-6 h-full py-4 items-center">
     @foreach ($tasks as $task)
         <x-todo.card-item :task="$task" />
     @endforeach
