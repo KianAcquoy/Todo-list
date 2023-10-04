0f5a295cb0e482dcf317638804016abe3bd19750
Kian Acquoij <kian.acquoij@student.rocva.nl>
2023-10-04 10:53:06 +0200

    Modal now changes size when page is getting bigger/smaller"

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -2,6 +2,11 @@ const modal = document.getElementById('modal');
 const modalClose = document.getElementById('closeModal');
 const modalContainer = document.getElementById('modalContainer');
 const iframe = document.getElementById('modalBody');
+const observer = new MutationObserver(handleMutations);
+
+function handleMutations(mutations) {
+    resizeIframe();
+}
 
 let minheight = 0;
 let maxheight = 80;
@@ -11,18 +16,29 @@ let width = 30;
     iframe.addEventListener('load', (e) => {
         e.preventDefault();
         modal.classList.remove('hidden');
-        iframe.style.width = `${document.body.scrollWidth / 100 * width}px`;
-        let height = iframe.contentWindow.document.body.scrollHeight;
-        if (minheight > 0 && height < minheight) {
-            height = window.top.innerHeight / 100 * minheight;
-        }
-        if (maxheight > 0 && height > window.top.innerHeight / 100 * maxheight) {
-            height = window.top.innerHeight / 100 * maxheight;
-        }
-        iframe.style.height = `${height}px`;
-        modalContainer.style.backgroundColor = window.getComputedStyle(iframe.contentWindow.document.body).backgroundColor;
+        resizeIframe();
+
+        observer.observe(iframe.contentDocument, {
+            subtree: true,
+            attributes: true,
+        });
+
+        window.addEventListener('resize', resizeIframe);
     });
 }())
+
+function resizeIframe() {
+    iframe.style.width = `${document.body.scrollWidth / 100 * width}px`;
+    let height = iframe.contentWindow.document.body.scrollHeight;
+    if (minheight > 0 && height < minheight) {
+        height = window.top.innerHeight / 100 * minheight;
+    }
+    if (maxheight > 0 && height > window.top.innerHeight / 100 * maxheight) {
+        height = window.top.innerHeight / 100 * maxheight;
+    }
+    iframe.style.height = `${height}px`;
+    modalContainer.style.backgroundColor = window.getComputedStyle(iframe.contentWindow.document.body).backgroundColor;
+}
 
 
 modalClose.addEventListener('click', () => {
@@ -49,14 +65,11 @@ export function modalPage(url = "", data = {}, settings = {}) {
     if (url === "") {
         return;
     } else {
-        minheight = settings.minheight ? settings.minheight : 0;
-        maxheight = settings.maxheight ? settings.maxheight : 80;
-        width = settings.width ? settings.width : 30;
         const queryParams = new URLSearchParams(data);
         url = url + "?" + queryParams.toString();
         iframe.setAttribute('src', url);
-        width = settings.width ? settings.width : width;
-        minheight = settings.minheight ? settings.minheight : minheight;
-        console.log('before load')
+        width = settings.width ? settings.width : 30;
+        minheight = settings.minheight ? settings.minheight : 0;
+        maxheight = settings.maxheight ? settings.maxheight : 80;
     }
 }
