df70729d16601258ee90c8a95598b49f874a3d86
Kian Acquoij <kian.acquoij@student.rocva.nl>
2023-10-04 10:53:40 +0200

    New styling for create task page

--- resources/views/components/todo/label.blade.php
+++ resources/views/components/todo/label.blade.php
@@ -1,7 +1,9 @@
-@props(['name', 'color', 'icon'])
+@props(['name', 'color', 'icon' => null])
 
 <div class="label px-4 w-fit rounded-xl flex space-x-1 items-center cursor-pointer" style="background-color: {{ $color }};">
-    @include('components.icons.' . $icon)
+    @if ($icon)
+        @include('components.icons.' . $icon)
+    @endif
     <div>
         {{ $name }}
     </div>
--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -1,35 +1,68 @@
 <x-popupweb-layout>
-    <x-slot name="header">
-        <h2 class="leading-tight">
-            {{ __('New task | ') . $card->name }}
-        </h2>
-    </x-slot>
+    <div class="p-2 space-y-8 w-[90vw]">
+        <div id="title" class="flex w-full">
+            <div class="px-4 flex justify-center items-center">
+                <x-icons.plus />
+            </div>
+            <div class="w-[50vw]">
+                <input type="text" name="name" id="name" class="w-full border border-gray-400 rounded px-2 py-1" placeholder="Enter name" value="New task">
+                <div class="text-md font-light">
+                    in <a class="underline underline-offset-2">{{ $card->name }}</a>
+                </div>
+            </div>
+        </div>
+        <div id="description" class="flex w-full">
+            <div class="px-4 flex justify-center items-center">
+                <x-icons.paragraph />
+            </div>
+            <div class="space-y-2 w-full">
+                <label for="description" class="font-semibold relative">
+                    Description
+                </label>
+                <textarea rows="5" name="description" id="description" class="w-full border border-gray-400 rounded px-2 py-1 resize-y" placeholder="Enter description"></textarea>
+            </div>
+        </div>
+        <div id="duedate" class="flex w-full">
+            <div class="px-4 flex justify-center items-center">
+                <x-icons.calendarcheck />
+            </div>
+            <div class="space-y-2">
+                <label for="due_date" class="font-semibold relative">
+                    Due date
+                </label>
+                <input type="date" id="due_date" name="due_date" class="w-full border border-gray-400 rounded px-2 py-1" placeholder="Enter name" value="New task">
+            </div>
+        </div>
 
-    <div class="py-8">
-        <div class="max-w-3xl mx-auto px-4">
-            <form action="{{ route('tasks.store') }}" method="POST">
-                @csrf
-                <input type="hidden" name="cardid" value="{{ $card->id }}">
-                <div class="mb-4">
-                    <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name</label>
-                    <input type="text" name="name" id="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('name') border-red-500 @enderror" value="{{ old('name') }}" placeholder="Enter name">
-                    @error('name')
-                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-4">
-                    <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Description</label>
-                    <textarea name="description" id="description" cols="30" rows="3" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none @error('description') border-red-500 @enderror" placeholder="Enter description">{{ old('description') }}</textarea>
-                    @error('description')
-                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="flex items-center justify-between">
-                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Create
-                    </button>
-                </div>
-            </form>
+
+        <div id="labels" class="flex w-full">
+            <?php 
+                $labels = [
+                    0 => [
+                        'name' => 'Bug',
+                        'color' => 'tomato'
+                    ],
+                    1 => [
+                        'name' => 'Feature',
+                        'color' => 'lightgreen'
+                    ],
+                    2 => [
+                        'name' => 'Enhancement',
+                        'color' => 'lightblue'
+                    ]
+                ];
+            ?>
+            <div class="px-4 flex justify-center items-center">
+                <x-icons.labels />
+            </div>
+            <div class="space-y-2">
+                @foreach ($labels as $id => $label)
+                    <div class="flex space-x-1 items-center">
+                        <input type="checkbox" name="label-{{ $id }}" id="label-{{ $id }}">
+                        <x-todo.label :name="$label['name']" :color="$label['color']" />
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </x-popupweb-layout>
